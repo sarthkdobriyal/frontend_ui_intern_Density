83d58dfc37c3f95d5fa98081da6956682210a3ac
root <root@DESKTOP-4GUT5LC.localdomain>
2023-05-31 12:24:36 +0530

    updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className='font-baloo'>
         <Header />
         {children}
